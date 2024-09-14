9805de5964df4212f5a38f1b46b16de56041497f
Ebo Arcadia <yiboli.coding@gmail.com>
2024-09-14 14:20:04 -0400

    inspect block chain

--- blockchain.go
+++ blockchain.go
@@ -12,7 +12,12 @@ const blocksBucket = "blocks"
 
 type Blockchain struct {
 	tip []byte
-	db *bolt.DB
+	db 	*bolt.DB
+}
+
+type BlockchainIterator struct {
+	currentHash []byte
+	db 			*bolt.DB
 }
 
 func (bc *Blockchain) AddBlock(data string) {
@@ -81,4 +86,23 @@ func NewBlockchain() *Blockchain {
 	return &bc
 }
 
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator {bc.tip, bc.db}
+	return bci
+}
+
+func (i *BlockchainIterator) next() *Block {
+	var block *Block
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+	i.currentHash = block.PrevBlockHash
+	return block
+}
 
